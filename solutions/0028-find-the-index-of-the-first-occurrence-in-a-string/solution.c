e7848946f0591e52bbae836d11984def4df463df
VardanKocharyan <vardanqocharyan5>
2025-04-19 08:34:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8 MB (100.00%)

---
+++ solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.c
@@ -0,0 +1,21 @@
+int strStr(char* haystack, char* needle)
+{
+    if (*needle == '\0') 
+    {
+        return 0;
+    }
+    for(int i = 0; *(haystack + i) != '\0' ; i++)
+    {
+        int idx = 0;
+        while(*(needle + idx) != '\0' && *(haystack + idx + i) == *(needle + idx))
+        {
+            idx++;
+        }
+        if(*(needle + idx) == '\0')
+        {
+            return i;
+        }
+        
+    }
+    return -1;
+}
