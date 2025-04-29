2b246beed8430d0486b6e9d1bd2172233777de2d
VardanKocharyan <202343946+VardanKocharyan@users.noreply.github.com>
2025-04-29 11:38:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.5 MB (100.00%)

---
+++ solutions/0674-longest-continuous-increasing-subsequence/solution.c
@@ -0,0 +1,28 @@
+int findLengthOfLCIS(int* nums, int numsSize) 
+{
+    int sum = 1;
+    int res  = 0;
+
+    if(numsSize == 1)
+    {
+        return sum;
+    }
+
+    for(int i = 0 ; i < numsSize - 1 ; i++)
+    {
+        if(*(nums + i) < *(nums + i + 1))
+        {
+            sum++;
+        }
+        else 
+        {
+            sum = 1;
+        }
+
+        if(res < sum)
+        {
+            res = sum;
+        }
+    }
+    return res;
+}
