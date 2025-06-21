2ec7bba46f031a8215d6309adc11a00337b6fdb0
VardanKocharyan <vardanqocharyan5>
2025-06-21 13:19:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (100.00%)

--- solutions/0009-palindrome-number/solution.c
+++ solutions/0009-palindrome-number/solution.c
@@ -1,9 +1,11 @@
 bool isPalindrome(int x) {
     int y = x;
     long int z = 0;
-    while(x > 0){
-        z = z*10  +  x%10;
-        x = x/10;
+
+    while (x > 0) {
+        z = z * 10  +  x % 10;
+        x = x / 10;
     }
+
     return y == z;
 }
