daf4687efab9ef789fcb6e06d2d0867face6dae8
VardanKocharyan <202343946+VardanKocharyan@users.noreply.github.com>
2025-09-09 08:02:19 +0000

    Sync LeetCode submission Runtime - 3 ms (45.38%), Memory - 9.4 MB (100.00%)

---
+++ solutions/0628-maximum-product-of-three-numbers/solution.c
@@ -0,0 +1,26 @@
+int maximumProduct(int* nums, int numsSize) {
+    int res1 = 1;
+    int res2 = 1;
+    for(int i = 0; i < 3; ++i) {
+        for(int j = i + 1; j < numsSize; ++j) {
+            if (nums[i] < nums[j] && nums[i] != nums[j]) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+        res1 *= nums[i]; 
+    }
+    res2 *= nums[0];
+    for(int i = 0; i < 2; ++i) {
+        for(int j = i + 1; j < numsSize; ++j) {
+            if (nums[i] > nums[j] && nums[i] != nums[j]) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+        res2 *= nums[i]; 
+    }
+    return (res1 > res2) ? res1 : res2;
+}
