5ef4e39397c06bc555143d435fca537fcc97e0d4
VardanKocharyan <vardanqocharyan5>
2026-04-11 07:58:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.4 MB (91.97%)

---
+++ solutions/0009-palindrome-number/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long int y = 0;
+
+        if(x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        while (x > y) {
+            y = y * 10  +  x % 10;
+            x = x / 10;
+        }
+
+        return (x == y || x == y/10);
+    }
+};
