664111d07b80e2dce539461fa5f6371f8c26395c
VardanKocharyan <vardanqocharyan5>
2026-04-14 09:22:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 35.6 MB (79.02%)

---
+++ solutions/0724-find-pivot-index/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum1{};
+        int sum2{};
+
+        if (nums.size() == 1) return 0;
+
+        for (int i{1}; i < nums.size(); ++i) {
+            sum2 += nums[i];
+        }
+
+        std::cout << std::endl;
+
+        for (int i{1}; i < nums.size(); ++i) {
+            if (sum1 == sum2) return i - 1;
+            sum1 += nums[i - 1];
+            sum2 -= nums[i];
+        }     
+
+        return sum1 == sum2 ? nums.size() - 1 : -1;
+    }
+};
