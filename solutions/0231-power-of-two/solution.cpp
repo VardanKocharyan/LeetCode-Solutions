2613630eddb2800bd8d49f0fc1b7859a2af6c0c4
VardanKocharyan <vardanqocharyan5>
2026-05-20 20:11:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (50.40%)

---
+++ solutions/0231-power-of-two/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 0 || n == std::numeric_limits<int>::max() || n == std::numeric_limits<int>::min()) return false;
+
+        return ((n - 1) & n) == 0;
+    }
+};
