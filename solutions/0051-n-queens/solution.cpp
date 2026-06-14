6d7f650010c8909579d76cca03d994de0c424d93
VardanKocharyan <202343946+VardanKocharyan@users.noreply.github.com>
2026-06-14 06:49:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.4 MB (47.47%)

---
+++ solutions/0051-n-queens/solution.cpp
@@ -0,0 +1,43 @@
+class Solution {
+std::vector<std::vector<std::string>> result;
+public:
+    bool isValid(std::vector<std::string>& board, int i, int j) {
+        int n = board.size();
+
+        for (int k{i - 1}; k >= 0; --k) {
+            if (board[k][j] == 'Q') return false;
+        }
+
+        for (int k{i - 1}, q{j - 1}; k >= 0 && q >= 0; --k, --q) {
+            if (board[k][q] == 'Q') return false;
+        }
+
+        for (int k{i - 1}, q{j + 1}; k >= 0 && q < n; --k, ++q) {
+            if (board[k][q] == 'Q') return false;
+        }
+
+        return true;
+    }
+
+    void nqueens(int n, std::vector<std::string>& board, int i) {
+        if (i >= n) {
+            result.push_back(board);
+            return;
+        }
+
+        for (int j{}; j < n; ++j) {
+            if (isValid(board, i, j)) {
+                board[i][j] = 'Q';
+                nqueens(n, board, i + 1);
+            }
+            board[i][j] = '.';
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        std::vector<std::string> board(n, std::string(n, '.'));
+        nqueens(n, board, 0);
+
+        return result;      
+    }
+};
