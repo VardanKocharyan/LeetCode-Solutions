d215ec210259c419e80599646f2389323b06873b
VardanKocharyan <202343946+VardanKocharyan@users.noreply.github.com>
2026-06-30 06:57:44 +0000

    Sync LeetCode submission Runtime - 70 ms (74.86%), Memory - 21.4 MB (68.73%)

--- solutions/1224-minimum-falling-path-sum-ii/solution.cpp
+++ solutions/1224-minimum-falling-path-sum-ii/solution.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    int solve(int day, int l_task, std::vector<std::vector<int>>& dp, std::vector<std::vector<int>>& g) {
-        if (dp[day][l_task] != 1e9) return dp[day][l_task];
-        int res = std::numeric_limits<int>::max();
-        if (day == 0) {
-            for (int task{}; task < g[0].size(); ++task) {
-                if (task ^ l_task) {
-                    res = std::min(res, g[day][task]);
-                }
-            }
-            return dp[day][l_task] = res;
-        }
-
-        for (int task{}; task < g[0].size(); ++task) {
-            if (task ^ l_task) {
-                res = std::min(res, solve(day - 1, task, dp, g) + g[day][task]);
-            }
-        }
-        return dp[day][l_task] = res;
-    }
-
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        std::vector<std::vector<int>> dp(n, std::vector<int>(m + 1, 1e9));
-        return solve(n - 1, m, dp, grid);
+        std::vector<int> dp(m, 1e9);
+        
+        dp = grid[0];
+
+        for (int day{1}; day < n; ++day) {
+            std::vector<int> curr(m);
+            for (int task{}; task < m; ++task) {
+                int best = std::numeric_limits<int>::max();
+                for (int prev{}; prev < m; ++prev) {
+                    if (prev ^ task) {
+                        best = std::min(best, dp[prev]);
+                    }
+                }
+                curr[task] = best + grid[day][task];
+            }
+            dp = std::move(curr);
+        }
+        return *std::min_element(dp.begin(), dp.end());
     }
 };
